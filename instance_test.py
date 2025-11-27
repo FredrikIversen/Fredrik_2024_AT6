d2447b77aa2265d315f151c7a3f5bf2c8590f43b
Fredrik Iversen <fredriko_ivers@hotmail.com>
2025-11-27 12:40:44 +0100

    Commit 3

--- instance_test.py
+++ instance_test.py
@@ -3,4 +3,19 @@ from user_input_validator import UserInputValidator
 validator1 = UserInputValidator()
 validator2 = UserInputValidator()
 
+list1 = ["34","32","0","fdas","43"]
+list2 = ("safw","2","-4","0","32")
 
+validator = UserInputValidator()
+
+
+# Instance 1
+a = validator.validate_positive_integers(list1)
+print(a)
+a2 = validator.validation_message(list1)
+
+
+# Instance 2
+b = validator.validate_positive_integers(list2)
+print(b)
+b2 = validator.validation_message(list2)
